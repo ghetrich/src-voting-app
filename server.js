3caf2b7c01b471ff5b8cae63309aa95746bec385
ghet <50469066+ghetrich@users.noreply.github.com>
2021-08-05 11:27:59 +0000

    Connected DB

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const path = require("path");
+require("dotenv").config();
 
+app.use(express.json());
 
-app.use(express.json())
-
-
-app.get("/", function (req, res) {
-    res.send({
-			name: "api",
-			version: "1.0.0",
-			description: "Tawiah SRC App",
-			main: "server.js",
-			scripts: {
-				test: 'echo "Error: no test specified" && exit 1',
-			},
-			keywords: ["Voting", "App"],
-			author: "rBOUARO",
-			license: "ISC",
-			dependencies: {
-				cors: "^2.8.5",
-				dotenv: "^10.0.0",
-				express: "^4.17.1",
-				gitignore: "^0.7.0",
-				nodemon: "^2.0.12",
-			},
+mongoose
+	.connect(process.env.DB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(result => {
+		console.log("connected to DB");
+		app.listen(process.env.PORT || 3030, () => {
+			console.log("Example app listening on port 3030!");
 		});
-});
+	})
 
-app.listen( process.env.PORT || 3030, () => {
-   console.log("Example app listening on port 3030!");   
-})
+app.use("register", require("./routes/register"))
+	
+
