3a95ccf34574f861652a5cc2dcea5afdb7de5349
ghet <50469066+ghetrich@users.noreply.github.com>
2021-08-05 11:58:24 +0000

    setup procfile again

--- server.js
+++ server.js
@@ -7,16 +7,20 @@ require("dotenv").config();
 app.use(express.json());
 
 mongoose
-	.connect(process.env.DB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
+	.connect(
+		process.env.DB_URI ||
+			"mongodb+srv://ghet:IT128411@cluster0.ayjeb.mongodb.net/gctu-src-voting-app",
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
 	.then(result => {
 		console.log("connected to DB");
 		app.listen(process.env.PORT || 3030, () => {
 			console.log("Example app listening on port 3030!");
 		});
-	})
+	});
 
 app.get('/', (req, res) => {
 	return res.send("Welcome! THIS GCTU SRC VOTING APP API")
