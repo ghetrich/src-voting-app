47147b3142cf0b5432edd253b0b4f68491fadb16
ghet <50469066+ghetrich@users.noreply.github.com>
2021-08-05 15:16:34 +0000

    nodemailer with oAuth2 modularized

---
+++ mailService.js
@@ -0,0 +1,45 @@
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
+
+const oAuth2Client = new google.auth.OAuth2(
+	process.env.CLIENT_ID,
+	process.env.CLIENT_SECRET,
+	process.env.REDIRECT_URL
+);
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+const sendMail = async (to, subject, text, html) => {
+	try {
+		const accessToken = await oAuth2Client.getAccessToken();
+
+		const transport = nodemailer.createTransport({
+			service: "gmail",
+			auth: {
+				type: "OAuth2",
+				user: "ghetrich370@gmail.com",
+				clientId: process.env.CLIENT_ID,
+				clientSecret: process.env.CLIENT_SECRET,
+				refreshToken: process.env.REFRESH_TOKEN,
+				accessToken: accessToken,
+			},
+		});
+
+		const mailOptions = {
+			from: "GCTU SRC GO MOBILE <ghetrich370@gmail.com>",
+			to,
+			subject,
+			text,
+			html,
+		};
+
+		const result = await transport.sendMail(mailOptions);
+
+		return result;
+	} catch (error) {
+		return error;
+	}
+};
+
+
+module.exports = sendMail;
--- routes/register.js
+++ routes/register.js
@@ -3,52 +3,9 @@ const Register = require("../models/register");
 const { nanoid } = require("nanoid");
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
+const {sendMail} = require("../mailService");
 
-const CLIENT_ID =
-	"643697278581-3s6j9dgshns4t8b1b5bf0ov81ggm0cum.apps.googleusercontent.com";
-const CLIENT_SECRET = "f3wHOsC8qvyx40NbZEyNOM6n";
-const REDIRECT_URL = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN =
-	"1//042SDWeqKZldcCgYIARAAGAQSNwF-L9IrQ7G7YfQT02ZQiC8ATcnrk3IT5EyLNpbO_flfDff7bzKAOJUQ12mOqv882j7_Lhezca8";
 
-const oAuth2Client = new google.auth.OAuth2(
-	CLIENT_ID,
-	CLIENT_SECRET,
-	REDIRECT_URL
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-const sendMail = async (to, subject, text, html) => {
-	try {
-		const accessToken = await oAuth2Client.getAccessToken();
-
-		const transport = nodemailer.createTransport({
-			service: "gmail",
-			auth: {
-				type: "OAuth2",
-				user: "ghetrich370@gmail.com",
-				clientId: CLIENT_ID,
-				clientSecret: CLIENT_SECRET,
-				refreshToken: REFRESH_TOKEN,
-				accessToken: accessToken,
-			},
-		});
-
-		const mailOptions = {
-			from: "GCTU SRC <ghetrich370@gmail.com>",
-			to,
-			subject,
-			text,
-			html,
-		};
-
-		const result = await transport.sendMail(mailOptions);
-
-		return result;
-	} catch (error) {
-		return error;
-	}
-};
 
 Router.post("/", (req, res) => {
 	const { othernames, surname, email, eligibility, campus, index } = req.body;
