317dff721243a41796eabdf89b397fa9e856f555
ghet <50469066+ghetrich@users.noreply.github.com>
2021-11-28 14:23:06 +0000

    results publication added

--- routes/position.js
+++ routes/position.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const Position = require("../models/position");
 const Election = require("../models/election");
@@ -12,18 +13,19 @@ app.post("/new/:electionId", async (req, res) => {
 		req.body;
 
 	if (!position || !about || candidates.length < 1) {
-		return res.status(400).send({ status:400, error: "Please provide all required information" });
+		return res.status(400).send({
+			status: 400,
+			error: "Please provide all required information",
+		});
 	}
 
 	// check whether the position is opened to all and if not make sure that allow group of voters are provided
 
 	if (!isGeneral && allowedVoterGroups.length < 1) {
-		return res
-			.status(400)
-			.send({
-				status: 400,
-				error: "Please indicate those allowed to vote for this position",
-			});
+		return res.status(400).send({
+			status: 400,
+			error: "Please indicate those allowed to vote for this position",
+		});
 	}
 
 	// check if election has started
@@ -60,26 +62,25 @@ app.post("/new/:electionId", async (req, res) => {
 							},
 						})
 							.then(done => {
-								return res.status(200).send({ status: 200 , msg:"Position successfully saved"});
+								return res.status(200).send({
+									status: 200,
+									msg: "Position successfully saved",
+								});
 							})
 							.catch(error => {
 								console.log(error);
-								return res
-									.status(500)
-									.send({
-										status: 500,
-										error: "Something went wrong saving position",
-									});
+								return res.status(500).send({
+									status: 500,
+									error: "Something went wrong saving position",
+								});
 							});
 					})
 					.catch(error => {
 						console.log(error);
-						return res
-							.status(500)
-							.send({
-								status: 500,
-								error: "Something went wrong saving position",
-							});
+						return res.status(500).send({
+							status: 500,
+							error: "Something went wrong saving position",
+						});
 					});
 			}
 		})
@@ -91,10 +92,71 @@ app.post("/new/:electionId", async (req, res) => {
 		});
 });
 
-app.get('/', async (req, res) => {
-
+app.put("/results/publish/:positionId/:electionId", async (req, res) => {
+	const positionId = req.params.positionId;
+	const electionId = req.params.electionId;
+	
 
 	
-})
+	if (positionId === undefined || positionId === null || electionId === null || electionId === undefined) {
+		return res.status(400).send({
+			status: 400,
+			error: "Cannot changed results state due to missing parameter(s)",
+		});
+	}
+
+	Election.findById(electionId)
+		.then(election => {
+		
+			if (
+				new Date(election.endsAt) > new Date() 
+			) {
+				return res.status(400).send({
+					status: 400,
+					error: "Cannot change results state because the election has not ended",
+				});
+			}
+
+			Position.findById(positionId)
+				.then(position => {
+					const isPublished = !position.isPublished;
+
+					Position.findByIdAndUpdate(
+						positionId,
+						{
+							$set: { isPublished: isPublished },
+						},
+						{ new: true }
+					)
+						.then(updatedPoisition => {
+							console.log(updatedPoisition);
+							return res.status(200).send({
+								status: 200,
+								msg: "Results state for this position has been updated",
+							});
+						})
+						.catch(error => {
+							console.log(error);
+							return res.status(500).send({
+								status: 500,
+								error: "Something went wrong while changing the result state",
+							});
+						});
+				})
+				.catch(error => {
+					console.log(error);
+					return res.status(500).send({
+						status: 500,
+						error: "Something went wrong with the server",
+					});
+				});
+		})
+		.catch(err => {
+			return res.status(500).send({
+				status: 500,
+				error: "Something went wrong with the server",
+			});
+		});
+});
 
 module.exports = app;
